eccb4c6433fd12b7b8086b578c7b95e38e329bfb
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-02-01 17:24:18 +0100

    first commit

---
+++ gitAdminstrator.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# At this script, we will interact with the application git
+# so it will be posible to clone, create, update...remotes or local
+# repositories.
+
+# asking if you want to create a new directory / path of work.
+# selecting a directory.
+
+
+# git init
+
+# git config
+# git config --list
+# git config --global user.name "Your name'
+# git config --global user.email "your email'
+
+
+# git clone
+# you need a user from git and the repository. It has to be valid.
+
+# git add
+
+# git commit
+# git commit -m "message'
+
+
+# git push
+# git push -u
+
+# git status
+
+
+# git pull
+
+# git branch
+
+# git checkout
+
+# git restore
+
+# git diff
+
+# rm ? 
+
+# git cache (git ignore)
